62ef1397ff7f9290ce35459251e2a40a6813f227
Dexter1521 <emtv2126@gmail.com>
2023-01-28 13:37:47 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,43 @@
 class ListaTareas{
-
     constructor(){
         this.tareas = JSON.parse(localStorage.getItem('tareas'));
-        if(!this.tareas){
-            this.tareas  = [
-                {tarea: 'Entregar reporte', completado: false},
-                {tarea: 'Jugar pc', completado: false},
-                {tarea: 'ir al cine', completado: true}
+
+        if (!this.tareas) {
+            this.tareas = [
+                {tarea: 'Aprender JavaScript', completado: false},
+                {tarea: 'Aprender Python', completado: false},
+                {tarea: 'Aprender C++', completado: false}
             ];
         }
-        this.caragarTareas();
+
+        this.cargarTareas();
+        this.agregarEventListeners();
     }
 
-    caragarTareas(){
+    agregarEventListeners() {
+        document.getElementById('recordatorio').addEventListener('keypress', (evento) => {
+            if(evento.keyCode == 13){
+                this.agregarTarea(evento.target.value);
+                evento.target.value = '';
+            }
+        });
+    }
+
+    cargarTareas() {
         localStorage.setItem('tareas', JSON.stringify(this.tareas));
-        let htmlTareas = this.tareas.reduce((html, tarea, indice) => html += this.generarHtmlTarea(tarea, indice, ''));
+        let htmlTareas = this.tareas.reduce((html, tarea, indice) => html += this.generarHtmlTarea(tarea, indice), '');
         document.getElementById('listaTareas').innerHTML = htmlTareas;
+    }
+
+    cambiarEstadoTarea(indice) {
+        this.tareas[indice].completado = !this.tareas[indice].completado;
+        this.cargarTareas();
+    }
+
+    eliminarTarea(evento, indice) {
+        evento.preventDefault();
+        this.tareas.splice(indice, 1);
+        this.cargarTareas();
     }
 
     generarHtmlTarea(tarea, indice) {
@@ -42,17 +64,36 @@ class ListaTareas{
         `;
     }
 
-    elimiinaTrea(evento, indice)
-    {
-        evento.prevenDefault();
-        this.tareas.splice(indice, 1);
-        this.caragarTareas
+    agregarTarea(tarea) {
+        let padre = document.getElementById('recordatorio').parentElement;
+
+        if (tarea !== ''){
+            padre.classList.remove('has-error');
+
+            let nuevaTarea = {
+                tarea,
+                completado: false
+            };
+
+            this.tareas.push(nuevaTarea);
+            this.cargarTareas();
+        } else {
+            padre.classList.add('has-error');
+        }
     }
 
+    agregarTareaClick() {
+        let recordatorio = document.getElementById('recordatorio');
+        let tarea = recordatorio.value;
+        if (tarea){
+            this.agregarTarea(tarea);
+            recordatorio.value = '';
+        }
+    }
 }
 
 let listaTareas;
 
-window.addEventListener('load', () =>{
+window.addEventListener('load', () => {
     listaTareas = new ListaTareas();
 })
